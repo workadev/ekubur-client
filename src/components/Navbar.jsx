ebb271229b6178d275a610fc50c9c14948448325
Ardhi Solikhin <asolikhin@fintax.id>
2025-11-14 14:56:22 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,76 @@
 import { Link } from "react-router-dom";
 import ekuburLogo from "../assets/images/ekubur-logo.svg";
 import menuLine from "../assets/images/icons/menu-line.svg";
+import { useEffect, useRef, useState } from "react";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  const menuItems = ["About Us", "Services", "Contact"];
+
+  // Detect click outside
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(e.target) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(e.target)
+      ) {
+        setOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
-    <nav className="navbar flex justify-between items-center py-3 px-4 md:px-16 md:py-6 bg-white shadow">
+    <div className="navbar flex justify-between items-center py-3 px-4 md:px-16 md:py-6 bg-white shadow">
       <img src={ekuburLogo} className="h-[68px]" />
 
       <ul className="hidden md:flex space-x-6 text-gray-700 font-medium">
-        <li>
-          <Link to={"/feature"} className="hover:text-[#A68A64]">
-            Features
-          </Link>
-        </li>
-        <li>
-          <Link to={"/contact"} className="hover:text-[#A68A64]">
-            Contact
-          </Link>
-        </li>
+        {
+          menuItems.map(item => (
+            <li>
+              <Link to={"/feature"} className="hover:text-[#A68A64]">
+                {item}
+              </Link>
+            </li>
+          ))
+        }
       </ul>
 
-      <button className="md:hidden flex items-center p-2 text-[#A68A64] text-[13px] md:text-[17px] font-medium">
-        Menu
-        <img src={menuLine} className="h-[14px] md:h-9 ml-1" />
-      </button>
-    </nav>
+      <div className="md:hidden relative">
+        <button 
+          ref={buttonRef}
+          onClick={() => setOpen(!open)}
+          className="flex items-center p-2 text-[#A68A64] text-[13px] md:text-[17px] font-medium transition-transform duration-300 gap-1"
+        >
+          <span>Menu</span>
+          <img src={menuLine} className="h-[14px]" />
+        </button>
+        <div
+          ref={menuRef}
+          className={`absolute right-0 overflow-hidden bg-white shadow-xl transition-all duration-500 ${open ? "max-h-96" : "max-h-0"}`}
+        >
+          <nav className="flex flex-col px-6 py-4 gap-4">
+            {menuItems.map((item, index) => (
+              <a
+                key={item}
+                href="#"
+                style={{ animationDelay: `${0.15 * index}s` }}
+                className={`text-[#7a6244] text-sm animate-slideDown min-w-max`}
+              >
+                {item}
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </div>
   );
 }
 
